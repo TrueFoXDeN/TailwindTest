45a954751d794bc81643a388e6b7998752d4187f
laurenz <laurenz.dilba@hotmail.de>
2024-05-21 20:58:17 +0200

    updated config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ButtonModule} from "primeng/button";
+import {DropdownModule} from "primeng/dropdown";
+import {FormsModule} from "@angular/forms";
+import {InputSwitchModule} from "primeng/inputswitch";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule],
+  imports: [RouterOutlet, ButtonModule, DropdownModule, FormsModule, InputSwitchModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
-  title = 'TailwindTest';
+  values = ["Wert 1", "Wert 2", "Wert 3"];
+  selected: any;
+  checked: any;
 }
